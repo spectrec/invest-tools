01e16b3c4b709db478cbc8fefeadc57b21446ac3
Vyacheslav Belous <v.belous@corp.mail.ru>
2018-09-16 21:16:31 +0300

    moex: stop if format changed

--- bond-listing/moex.go
+++ bond-listing/moex.go
@@ -50,7 +50,21 @@ const (
 	moexCsvOtherSecurities
 	moexCsvDisclosurePartPage
 	moexCsvDisclosureRfInfoPage
+	moexCsvLast
 )
+
+var expectedOrder = []string{
+	"DATESTAMP", "INSTRUMENT_ID", "LIST_SECTION", "RN", "SUPERTYPE",
+	"INSTRUMENT_TYPE", "INSTRUMENT_CATEGORY", "TRADE_CODE", "ISIN",
+	"REGISTRY_NUMBER", "REGISTRY_DATE", "EMITENT_FULL_NAME", "INN",
+	"NOMINAL", "CURRENCY", "ISSUE_AMOUNT", "DECISION_DATE",
+	"OKSM_EDR", "ONLY_EMITENT_FULL_NAME", "REG_COUNTRY", "QUALIFIED_INVESTOR",
+	"HAS_PROSPECTUS", "IS_CONCESSION_AGREEMENT", "IS_MORTGAGE_AGENT", "INCLUDED_DURING_CREATION",
+	"SECURITY_HAS_DEFAULT", "SECURITY_HAS_TECH_DEFAULT", "INCLUDED_WITHOUT_COMPLIANCE", "RETAINED_WITHOUT_COMPLIANCE",
+	"HAS_RESTRICTION_CIRCULATION", "LISTING_LEVEL_HIST", "OBLIGATION_PROGRAM_RN", "COUPON_PERCENT",
+	"EARLY_REPAYMENT", "EARLY_REDEMPTION", "ISS_BOARDS", "OTHER_SECURITIES",
+	"DISCLOSURE_PART_PAGE", "DISCLOSURE_RF_INFO_PAGE",
+}
 
 type moexBond struct {
 	Name           string
@@ -61,6 +75,7 @@ type moexBond struct {
 
 func moexDownloadAndParse() map[string]moexBond {
 	result := make(map[string]moexBond)
+	headerChecked := false
 
 	resp, err := http.Get("https://www.moex.com/ru/listing/securities-list-csv.aspx?type=1")
 	if err != nil {
@@ -79,7 +94,27 @@ func moexDownloadAndParse() map[string]moexBond {
 			log.Fatal(err)
 		}
 
+		if !headerChecked {
+			headerChecked = true
+			if len(parsed) < len(expectedOrder) {
+				log.Fatal("header changed, too short: ", parsed)
+			}
+
+			for i := range expectedOrder {
+				if parsed[i] == expectedOrder[i] {
+					continue
+				}
+
+				log.Fatalf("moex order changed: expected `%s', got `%s'",
+					expectedOrder[i], parsed[i])
+			}
+		}
+
 		// Exclude bad strings
+		if len(parsed) < int(moexCsvLast) {
+			log.Println("skip bad csv: ", parsed)
+			continue
+		}
 		if parsed[moexCsvISIN] == "" {
 			continue
 		}
