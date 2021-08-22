8862ae8a7d3831cb21ed22bf4f1f3415759282a2
Vyacheslav Belous <v.belous@corp.mail.ru>
2021-08-22 15:39:38 +0300

    cmd/financials: add `per share' stat + add percents for some rows

--- cmd/financials/main.go
+++ cmd/financials/main.go
@@ -56,6 +56,29 @@ type financials struct {
 	Comments string `yaml:"comments"`
 }
 
+func convert(data []financials) {
+	for i := range data {
+		const balanceNormalizer = 1e6
+
+		data[i].Exchange.CapitalizationPriv = data[i].Exchange.CapitalizationPriv / balanceNormalizer
+		data[i].Exchange.CapitalizationRegular = data[i].Exchange.CapitalizationRegular / balanceNormalizer
+
+		data[i].Assets = data[i].Assets * data[i].Multiplier / balanceNormalizer
+		data[i].CashAndEquivalents = data[i].CashAndEquivalents * data[i].Multiplier / balanceNormalizer
+		data[i].Liabilities = data[i].Liabilities * data[i].Multiplier / balanceNormalizer
+		data[i].LongTermDebt = data[i].LongTermDebt * data[i].Multiplier / balanceNormalizer
+		data[i].ShortTermDebt = data[i].ShortTermDebt * data[i].Multiplier / balanceNormalizer
+		data[i].Equity = data[i].Equity * data[i].Multiplier / balanceNormalizer
+
+		data[i].Revenue = data[i].Revenue * data[i].Multiplier / balanceNormalizer
+		data[i].OperatingIncome = data[i].OperatingIncome * data[i].Multiplier / balanceNormalizer
+		data[i].InterestExpences = data[i].InterestExpences * data[i].Multiplier / balanceNormalizer
+		data[i].NetIncome = data[i].NetIncome * data[i].Multiplier / balanceNormalizer
+
+		data[i].Dividents = data[i].Dividents * data[i].Multiplier / balanceNormalizer
+	}
+}
+
 func main() {
 	if len(os.Args) != 3 {
 		fmt.Fprintf(os.Stderr, "Usage: %v <config> <financials>\n", os.Args[0])
@@ -86,136 +109,157 @@ func main() {
 		os.Exit(1)
 	}
 
+	convert(financials)
+
 	analyze(os.Stdout, financials, conf)
 }
 
-func analyze(w io.Writer, data []financials, conf config) {
-	const columnNameFmt = "%- 40s"
-	const columnDataFmt = "% 15.2f"
+type printOptions struct {
+	rowNameFmt string
 
+	dataFmt              string
+	dataChangePercentFmt string
+
+	showDataChangePercent bool
+}
+
+func printRow(w io.Writer, rowName string, opts printOptions, n int, getValue func(i int) float64) {
+	fmt.Fprintf(w, opts.rowNameFmt, rowName)
+
+	for i := 0; i < n; i++ {
+		v := getValue(i)
+
+		fmt.Fprintf(w, opts.dataFmt, v)
+		if opts.showDataChangePercent {
+			if i == 0 {
+				fmt.Fprintf(w, opts.dataChangePercentFmt, 0.0)
+			} else {
+				prev := getValue(i - 1)
+				fmt.Fprintf(w, opts.dataChangePercentFmt, (v-prev)/prev*100.0)
+			}
+		}
+	}
+	fmt.Fprintf(w, "\n")
+}
+
+func calcROE(data []financials) (roe []float64, avg float64) {
+	for i := range data {
+		var v float64
+
+		if i != 0 {
+			v = data[i].NetIncome / data[i-1].Equity * 100.0
+			avg += v
+		}
+
+		roe = append(roe, v)
+	}
+	avg /= float64(len(data) - 1)
+
+	return roe, avg
+}
+
+func analyze(w io.Writer, data []financials, conf config) {
 	if len(data) == 0 {
 		return
 	}
 
-	fmt.Fprintf(w, columnNameFmt, "Год")
-	for _, r := range data {
-		fmt.Fprintf(w, "% 15d", r.Year)
-	}
-	fmt.Fprintf(w, "\n\n")
+	const columnNameFmt = "%- 40s"
+	const columnDataFmt = "% 15.1f"
+	const columnDataChangePercent = " (%+ 4.0f%%)"
 
-	const balanceNormalizer = 1e9
-	fmt.Fprintf(w, columnNameFmt, "Активы (млрд)")
-	for _, r := range data {
-		fmt.Fprintf(w, columnDataFmt, r.Assets*r.Multiplier/balanceNormalizer)
-	}
+	opts := printOptions{rowNameFmt: columnNameFmt, dataFmt: "% 21.0f"}
+	printRow(w, "Год", opts, len(data), func(i int) float64 { return float64(data[i].Year) })
 	fmt.Fprintf(w, "\n")
 
-	fmt.Fprintf(w, columnNameFmt, "Обязательства (млрд)")
-	for _, r := range data {
-		fmt.Fprintf(w, columnDataFmt, r.Liabilities*r.Multiplier/balanceNormalizer)
+	opts = printOptions{
+		rowNameFmt:            columnNameFmt,
+		dataFmt:               columnDataFmt,
+		dataChangePercentFmt:  columnDataChangePercent,
+		showDataChangePercent: true,
 	}
+	printRow(w, "Активы (млн)", opts, len(data), func(i int) float64 { return data[i].Assets })
+	printRow(w, "Обязательства (млн)", opts, len(data), func(i int) float64 { return data[i].Liabilities })
+	printRow(w, "Собственный капитал (млн)", opts, len(data), func(i int) float64 { return data[i].Equity })
+	printRow(w, "Собственный капитал per share", opts, len(data), func(i int) float64 {
+		return data[i].Equity * 1e6 / float64(data[i].Exchange.StockCountPriv+data[i].Exchange.StockCountRegular)
+	})
 	fmt.Fprintf(w, "\n")
 
-	fmt.Fprintf(w, columnNameFmt, "Собственный капитал (млрд)")
-	for _, r := range data {
-		fmt.Fprintf(w, columnDataFmt, r.Equity*r.Multiplier/balanceNormalizer)
-	}
+	printRow(w, "Выручка (млн)", opts, len(data), func(i int) float64 { return data[i].Revenue })
+	printRow(w, "Операционная прибыль (млн)", opts, len(data), func(i int) float64 { return data[i].OperatingIncome })
+	printRow(w, "Чистая прибыль (млн)", opts, len(data), func(i int) float64 { return data[i].NetIncome })
 	fmt.Fprintf(w, "\n")
 
-	fmt.Fprintf(w, columnNameFmt, "Капитализация (млрд)")
-	for _, r := range data {
-		fmt.Fprintf(w, columnDataFmt, (r.Exchange.CapitalizationPriv+r.Exchange.CapitalizationRegular)/balanceNormalizer)
-	}
-	fmt.Fprintf(w, "\n\n")
-
-	const resultsNormalizer = 1e6
-	fmt.Fprintf(w, columnNameFmt, "Выручка (млн)")
-	for _, r := range data {
-		fmt.Fprintf(w, columnDataFmt, r.Revenue*r.Multiplier/resultsNormalizer)
-	}
+	printRow(w, "Выручка per share", opts, len(data), func(i int) float64 {
+		return data[i].Revenue * 1e6 / float64(data[i].Exchange.StockCountPriv+data[i].Exchange.StockCountRegular)
+	})
+	printRow(w, "Операционная прибыль per share", opts, len(data), func(i int) float64 {
+		return data[i].OperatingIncome * 1e6 / float64(data[i].Exchange.StockCountPriv+data[i].Exchange.StockCountRegular)
+	})
+	printRow(w, "Чистая прибыль per share", opts, len(data), func(i int) float64 {
+		return data[i].NetIncome * 1e6 / float64(data[i].Exchange.StockCountPriv+data[i].Exchange.StockCountRegular)
+	})
 	fmt.Fprintf(w, "\n")
 
-	fmt.Fprintf(w, columnNameFmt, "Операционная прибыль (млн)")
-	for _, r := range data {
-		fmt.Fprintf(w, columnDataFmt, r.OperatingIncome*r.Multiplier/resultsNormalizer)
-	}
-	fmt.Fprintf(w, "\n")
+	printRow(w, "Выплаченные дивиденды (млн)", opts, len(data), func(i int) float64 { return data[i].Dividents })
+	printRow(w, "Дивиденды per share", opts, len(data), func(i int) float64 {
+		return data[i].Dividents * 1e6 / (data[i].Exchange.StockCountPriv + data[i].Exchange.StockCountRegular)
+	})
 
-	fmt.Fprintf(w, columnNameFmt, "Процентные расходы (млн)")
-	for _, r := range data {
-		fmt.Fprintf(w, columnDataFmt, r.InterestExpences*r.Multiplier/resultsNormalizer)
-	}
-	fmt.Fprintf(w, "\n")
+	opts.dataFmt = "% 21.2f"
+	opts.showDataChangePercent = false
 
-	fmt.Fprintf(w, columnNameFmt, "Чистая прибыль (млн)")
-	for _, r := range data {
-		fmt.Fprintf(w, columnDataFmt, r.NetIncome*r.Multiplier/resultsNormalizer)
-	}
-	fmt.Fprintf(w, "\n")
-
-	fmt.Fprintf(w, columnNameFmt, "Выплаченные дивиденды (млн)")
-	for _, r := range data {
-		fmt.Fprintf(w, columnDataFmt, r.Dividents*r.Multiplier/resultsNormalizer)
-	}
-	fmt.Fprintf(w, "\n\n")
-
-	fmt.Fprintf(w, columnNameFmt, "Закредитованность (Обязательства/Активы)")
-	for _, r := range data {
-		fmt.Fprintf(w, columnDataFmt, r.Liabilities/r.Assets)
-	}
-	fmt.Fprintf(w, "\n")
-
-	fmt.Fprintf(w, columnNameFmt, "ROE")
-	var roe_avg float64
-	for i := range data {
-		if i == 0 {
-			fmt.Fprintf(w, columnDataFmt, 0.0)
-			continue
+	printRow(w, "Дивидендная доходность (ап)", opts, len(data), func(i int) float64 {
+		count := data[i].Exchange.StockCountPriv
+		if count == 0.0 {
+			return 0.0
 		}
 
-		roe := data[i].NetIncome / data[i-1].Equity * 100.0
-		roe_avg += roe
+		price := data[i].Exchange.CapitalizationPriv / count
+		dividendsPerShare := data[i].Dividents / (data[i].Exchange.StockCountPriv + data[i].Exchange.StockCountRegular)
 
-		fmt.Fprintf(w, columnDataFmt, roe)
-	}
-	roe_avg /= float64(len(data) - 1)
-	fmt.Fprintf(w, "\n")
-
-	fmt.Fprintf(w, columnNameFmt, "ROS")
-	for _, r := range data {
-		fmt.Fprintf(w, columnDataFmt, r.NetIncome/r.Revenue*100.0)
-	}
-	fmt.Fprintf(w, "\n")
-
-	fmt.Fprintf(w, columnNameFmt, "Выручка/Активы")
-	for i := range data {
-		if i == 0 {
-			fmt.Fprintf(w, columnDataFmt, 0.0)
-			continue
+		return dividendsPerShare / price * 100
+	})
+	printRow(w, "Дивидендная доходность (аo)", opts, len(data), func(i int) float64 {
+		count := data[i].Exchange.StockCountRegular
+		if count == 0.0 {
+			return 0.0
 		}
 
-		fmt.Fprintf(w, columnDataFmt, data[i].Revenue/data[i-1].Assets)
-	}
-	fmt.Fprintf(w, "\n")
+		price := data[i].Exchange.CapitalizationRegular / count
+		dividendsPerShare := data[i].Dividents / (data[i].Exchange.StockCountPriv + data[i].Exchange.StockCountRegular)
+
+		return dividendsPerShare / price * 100
+	})
 
 	var tooHighPayoutRatio = false
-	fmt.Fprintf(w, columnNameFmt, "Payout ratio")
-	for _, r := range data {
-		payoutRatio := r.Dividents / r.NetIncome * 100.0
-		fmt.Fprintf(w, columnDataFmt, payoutRatio)
-
+	printRow(w, "Payout ratio", opts, len(data), func(i int) float64 {
+		payoutRatio := data[i].Dividents / data[i].NetIncome * 100.0
 		if payoutRatio > 100.0 {
 			tooHighPayoutRatio = true
 		}
-	}
+		return payoutRatio
+	})
 	fmt.Fprintf(w, "\n")
 
-	fmt.Fprintf(w, columnNameFmt, "E/P")
-	for _, r := range data {
-		capitalization := r.Exchange.CapitalizationPriv + r.Exchange.CapitalizationRegular
-		fmt.Fprintf(w, columnDataFmt, r.NetIncome*r.Multiplier/capitalization*100.0)
-	}
-	fmt.Fprintf(w, "\n\n")
+	printRow(w, "Закредитованность (Обязательства/Активы)", opts, len(data), func(i int) float64 { return data[i].Liabilities / data[i].Assets })
+
+	roe, roe_avg := calcROE(data)
+	printRow(w, "ROE", opts, len(data), func(i int) float64 { return roe[i] })
+	printRow(w, "ROS", opts, len(data), func(i int) float64 { return data[i].NetIncome / data[i].Revenue })
+	printRow(w, "Выручка/Активы", opts, len(data), func(i int) float64 {
+		if i == 0 {
+			return 0.0
+		}
+
+		return data[i].Revenue / data[i].Assets
+	})
+
+	printRow(w, "E/P", opts, len(data), func(i int) float64 {
+		capitalization := data[i].Exchange.CapitalizationPriv + data[i].Exchange.CapitalizationRegular
+		return data[i].NetIncome / capitalization * 100.0
+	})
+	fmt.Fprintf(w, "\n")
 
 	for _, r := range data {
 		if len(r.Comments) != 0 {
@@ -229,66 +273,27 @@ func analyze(w io.Writer, data []financials, conf config) {
 
 	var lastYear = data[len(data)-1]
 	fmt.Fprintf(w, "\tРазмер компании:\n")
-	{
-		if lastYear.Assets*lastYear.Multiplier >= conf.MinAssets {
-			fmt.Fprintf(w, "\t\tАктивы: ок\n")
-		} else {
-			fmt.Fprintf(w, "\t\tАктивы: не ок\n")
-		}
-
-		if lastYear.Revenue*lastYear.Multiplier >= conf.MinRevenue {
-			fmt.Fprintf(w, "\t\tВыручка: ок\n")
-		} else {
-			fmt.Fprintf(w, "\t\tВыручка: не ок\n")
-		}
-	}
+	fmt.Fprintf(w, "\t\tАктивы: %.1f млн (required min: %v)\n", lastYear.Assets, conf.MinAssets)
+	fmt.Fprintf(w, "\t\tВыручка: %.1f млн (required min: %v)\n", lastYear.Revenue, conf.MinRevenue)
 
 	fmt.Fprintf(w, "\tУстойчивость компании:\n")
-	{
-		var liabilitiesToAssets = lastYear.Liabilities / lastYear.Assets
-		if liabilitiesToAssets <= conf.MaxLiabilitiesToAssets {
-			fmt.Fprintf(w, "\t\tЗакредитованность: ок (%.2f)\n", liabilitiesToAssets)
-		} else {
-			if lastYear.IsBank {
-				fmt.Fprintf(w, "\t\tЗакредитованность: не ок (%.2f), но это банк, поэтому можно пропустить\n", liabilitiesToAssets)
-			} else {
-				fmt.Fprintf(w, "\t\tЗакредитованность: не ок (%.2f)\n", liabilitiesToAssets)
-			}
-		}
-
-		var debtToEquity = (lastYear.ShortTermDebt + lastYear.LongTermDebt) / lastYear.Equity
-		if debtToEquity > conf.MaxDebtToEquity {
-			fmt.Fprintf(w, "\t\tДолг/Капитал: не ок (%.2f)\n", debtToEquity)
-		} else {
-			fmt.Fprintf(w, "\t\tДолг/Капитал: ок (%.2f)\n", debtToEquity)
-		}
-
-		var opResToInterestEx = lastYear.OperatingIncome / lastYear.InterestExpences
-		if opResToInterestEx < conf.MinOperatingIncomeToInterestExpences {
-			fmt.Fprintf(w, "\t\tОперационная прибыль/Проценты к уплате: не ок (%.2f)\n", opResToInterestEx)
-		} else {
-			fmt.Fprintf(w, "\t\tОперационная прибыль/Проценты к уплате: ок (%.2f)\n", opResToInterestEx)
-		}
-
-		if tooHighPayoutRatio {
-			fmt.Fprintf(w, "\t\tPayout ratio > 100%: не ок\n")
-		}
-	}
+	fmt.Fprintf(w, "\t\tЗакредитованность: %.2f, bank: %v (required max: %v)\n",
+		lastYear.Liabilities/lastYear.Assets, lastYear.IsBank, conf.MaxLiabilitiesToAssets)
+	fmt.Fprintf(w, "\t\tДолг/Капитал: %.2f, bank: %v (required max: %v)\n",
+		(lastYear.ShortTermDebt+lastYear.LongTermDebt)/lastYear.Equity, lastYear.IsBank, conf.MaxDebtToEquity)
+	fmt.Fprintf(w, "\t\tОперационная прибыль/Проценты к уплате: %.2f, bank: %v (required min: %v)\n",
+		lastYear.OperatingIncome/lastYear.InterestExpences, lastYear.IsBank, conf.MinOperatingIncomeToInterestExpences)
 
 	fmt.Fprintf(w, "\tЭффективность и стоимость:\n")
 	{
-		if roe_avg >= conf.MinROE {
-			fmt.Fprintf(w, "\t\tROE (avg): ок (%.2f)\n", roe_avg)
-		} else {
-			fmt.Fprintf(w, "\t\tROE (avg): не ок (%.2f)\n", roe_avg)
-		}
+		fmt.Fprintf(w, "\t\tROE (avg): %.2f (required min: %v)\n", roe_avg, conf.MinROE)
 
 		capitalization := lastYear.Exchange.CapitalizationPriv + lastYear.Exchange.CapitalizationRegular
-		fmt.Fprintf(w, "\t\tТекущая доходность (E/P): %.2f%%\n", lastYear.NetIncome*lastYear.Multiplier/capitalization*100)
+		fmt.Fprintf(w, "\t\tТекущая доходность (E/P): %.2f%%\n", lastYear.NetIncome/capitalization*100)
 
 		// equity*roe == expected net income
-		expectedCapitalization := lastYear.Equity * lastYear.Multiplier * roe_avg / conf.ExpectedReturn
-		k := expectedCapitalization / capitalization
+		expectedCapitalization := lastYear.Equity * roe_avg / conf.ExpectedReturn
+		k := expectedCapitalization / capitalization * 1e6
 		fmt.Fprintf(w, "\t\tОценка по Арсагере (r=%.2f): ao=%.2f ап=%.2f\n", conf.ExpectedReturn,
 			lastYear.Exchange.CapitalizationRegular/lastYear.Exchange.StockCountRegular*k,
 			lastYear.Exchange.CapitalizationPriv/lastYear.Exchange.StockCountPriv*k)
